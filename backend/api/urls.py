815e265df1889ff94ebcc112c1672e431f31a355
alixxxx-x <alix20041124@gmail.com>
2025-12-23 22:52:41 +0100

    new

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('token/get', TokenObtainPairView.as_view(), name='get_token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('user/register/', UserCreateView.as_view(), name='register'),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('events/', EventListCreate.as_view(), name='event_list_create'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -8,4 +8,25 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
     
+class EventListCreate(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        user = self.request.user
+        return Event.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print("Serializer errors:", serializer.errors)        
+class EventDelete(generics.DestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Event.objects.filter(author=user)
