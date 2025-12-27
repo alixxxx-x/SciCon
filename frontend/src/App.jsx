fb8621cae8b0aea600deedf073ad47c858d6c23b
alixxxx-x <alix20041124@gmail.com>
2025-12-27 05:53:08 +0100

    made some updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
 import react from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import NotFound from "./pages/notfound";    
+import ProtectedRoutes from "./components/ProtectedRoutes";
+
+function Logout () {
+    localStorage.clear();
+    return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+    localStorage.clear();
+    return <Register />;
+}
 
 function App () {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<ProtectedRoutes><Home /></ProtectedRoutes>} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/register" element={<RegisterAndLogout />} />
+                <Route path="/logout" element={<Logout />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -2,16 +2,50 @@ import { Navigate } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACESS_TOKEN } from "../constants";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 
 function ProtectedRoutes({ children}) {
     const [isAuthorized, setIsAuthorized] = useState(null);
 
+    useEffect(() => { checkAuth().catch(() => setIsAuthorized(false)) }, [])
+
     const refreshToken = async () => {
-        
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const response = await api.post("api/token/refresh/", { refresh: refreshToken });
+
+            if (response.status === 200) {
+                localStorage.setItem(ACESS_TOKEN, response.data.access);
+                setIsAuthorized(true);
+            }
+            else {
+                setIsAuthorized(false);
+            }
+
+
+        } catch (error) {
+            console.error("Error refreshing token:", error);
+            setIsAuthorized(false);
+        }
     }
 
     const checkAuth = async () => {
+        const token = localStorage.getItem(ACESS_TOKEN);
+        if(!token) {
+            setIsAuthorized(false);
+            return;
+        }
+
+        const decodedToken = jwtDecode(token);
+        const tokenExpiration = decodedToken.exp;
+        const currentTime = Date.now() / 1000;
+
+        if (tokenExpiration < currentTime) {
+            await refreshToken();
+        } else {
+            setIsAuthorized(true);
+        }
+            
 
     }
 
@@ -21,3 +55,5 @@ function ProtectedRoutes({ children}) {
 
     return isAuthorized ? children : <Navigate to="/login" />;
 }
+
+export default ProtectedRoutes;
