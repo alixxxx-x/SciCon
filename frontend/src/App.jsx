5b1f3d8777be7fe972c61b8846eda7894c1c03f3
nadjjib <nnaaddjjiibb20@gmail.com>
2026-01-04 02:23:56 +0100

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import react from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import NotFound from "./pages/notfound";    
+import NotFound from "./pages/notfound";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 
-function Logout () {
+function Logout() {
     localStorage.clear();
     return <Navigate to="/login" />;
 }
@@ -16,11 +17,12 @@ function RegisterAndLogout() {
     return <Register />;
 }
 
-function App () {
+function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<ProtectedRoutes><Home /></ProtectedRoutes>} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<RegisterAndLogout />} />
                 <Route path="/logout" element={<Logout />} />
